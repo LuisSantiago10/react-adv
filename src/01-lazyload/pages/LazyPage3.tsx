d937fd10505d988e403cc660ea5c450ffc4246fe
Luis Santiago <lux_luis10@hotmail.com>
2022-04-12 20:56:18 -0500

    LazyLoad Basico listo

--- src/01-lazyload/pages/LazyPage3.tsx
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -3,3 +3,4 @@ export const LazyPage3 = () => {
     <h1>Lazy page 3</h1>
   )
 }
+export default LazyPage3;
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -5,32 +5,40 @@ import {
   NavLink
 } from 'react-router-dom';
 
-import { LazyPage1,LazyPage2,LazyPage3  } from '../01-lazyload/pages'
-
+// import { LazyPage1,LazyPage2,LazyPage3  } from '../01-lazyload/pages'
+import { routes } from './routes';
+import { Suspense } from 'react';
 import logo from '../logo.svg';
 
 export const Navigation = () => {
   return (
+    <Suspense fallback={ <span>Loading...</span>}>
     <Router>
       <div className="main-layout">
         <nav>
             <img src={ logo } alt="React Logo" />
           <ul>
-            <li>
-              <NavLink to="/lazy1" activeClassName="nav-active" exact>Lazy1</NavLink>
-            </li>
-            <li>
+            {  
+              routes.map( ({to,name}) => (
+                <li key={ to }>
+                  <NavLink to={ to } activeClassName="nav-active" exact>{ name }</NavLink>
+                </li>
+              )) 
+            }
+            {/* <li>
               <NavLink to="/lazy2" activeClassName="nav-active" exact>Lazy2</NavLink>
             </li>
             <li>
               <NavLink to="/lazy3" activeClassName="nav-active" exact>Lazy3</NavLink>
-            </li>
+            </li> */}
           </ul>
         </nav>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
+          {
+          /*
           <Route path="/lazy1">
             <LazyPage1 />
           </Route>
@@ -40,11 +48,22 @@ export const Navigation = () => {
           <Route path="/lazy3">
           <LazyPage3 />
           </Route>
-          <Route path="/*">
-            <h1>Home2</h1>
-          </Route>
+          */
+          }
+
+          {
+            routes.map( ({to,path,Component}) => (
+                <Route 
+                  key={path} 
+                  path={ to }>
+                  <Component />
+                </Route>
+            ))
+          }
+          {/* <Route path="/*" component={routes[0].to}>  </Route> */}
         </Switch>
       </div>
     </Router>
+    </Suspense>
   );
 }
